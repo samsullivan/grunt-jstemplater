be06876202483bb1b22842cf40e188968a772553
Sam Sullivan <samsullivan09@gmail.com>
2015-01-29 14:43:23 -0600

    use files.src/dest for building multiple templates

--- tasks/template.js
+++ tasks/template.js
@@ -58,26 +58,26 @@ module.exports = function(grunt) {
 	// ==========================================================================
 
 	grunt.registerMultiTask('template', 'Concatenate all javascript templates into json.', function() {
-		var files = grunt.file.expand(this.filesSrc);
 		var staticPath = this.data.variables.staticPath ? this.data.variables.staticPath : 'javascripts/templates/';
-		staticPath.charAt( staticPath.length - 1 ) !== '/' ? staticPath += '/' : void 0;
-		var src = template(files, staticPath);
-		var obj = {};
-		var name = 'TEMPLATES';
-		
-		for( item in src ) {
-			src[item] = src[item].replace(/(\r\n|\n|\r|\t)/gm,"");
-			src[item] = src[item].replace(/>[\s]+</gm,"><");
-			src[item] = src[item].replace(/[\s]{2,}/gm,"");
-			obj = deepmerge(obj, JSON.parse(src[item]));
-		}
-		
-		if( this.data.variables.name && this.data.variables.name !== '' )
-			name = this.data.variables.name
-		
-		grunt.file.write(this.data.dest, 'var '+ name +' = ' + JSON.stringify(obj) + ';');
 
-		grunt.log.ok(this.data.dest, name);
+		this.data.files.forEach(function(f) {
+			staticPath = f.staticPath ? f.staticPath : staticPath;
+			staticPath.charAt(staticPath.length - 1) !== '/' ? staticPath += '/' : void 0;
+
+			var files = grunt.file.expand(f.src),
+				src = template(files, staticPath),
+				obj = {};
+
+			for(var item in src) {
+				src[item] = src[item].replace(/(\r\n|\n|\r|\t)/gm, "");
+				src[item] = src[item].replace(/>[\s]+</gm, "><");
+				src[item] = src[item].replace(/[\s]{2,}/gm, "");
+				obj = deepmerge(obj, JSON.parse(src[item]));
+			}
+
+			grunt.file.write(f.dest, 'var ' + f.name + ' = ' + JSON.stringify(obj) + ';');
+			grunt.log.ok(f.dest);
+		});
 	});
 
 };
